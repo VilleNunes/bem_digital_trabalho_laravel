fe757cffa4d3e16e91c4e464c12b33253dc591ec
Peridan <danielsatelpereira@gmail.com>
2025-10-14 20:18:50 -0300

    atualizacao navbar e topbar e outros elementos
    eu e ville mexemos

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,4 +1,4 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-200 focus:border-indigo-200
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-200
 focus:ring-indigo-500 placeholder-gray-400 placeholder:font-normal rounded-md shadow-sm']) }}>
