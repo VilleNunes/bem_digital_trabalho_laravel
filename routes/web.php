9dd9efdad47ce0cece38d8b3974b6af52fd0b6ea
Rafaelcolombo23 <99507920+Rafaelcolombo23@users.noreply.github.com>
2025-10-20 00:44:49 -0300

    criando as telas de listagem e edicoes de instuicao

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\InstitutionController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
@@ -21,16 +22,24 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // Routes users
-    Route::get('/users',[UsersController::class,'index'])->name('users.index');
-    Route::get('/users/create',[UsersController::class,'create'])->name('users.create');
-    Route::get('/users/{user}/show',[UsersController::class,'show'])->name('users.show');
-    Route::post('/users/create',[UsersController::class,'store'])->name('users.store');
-    Route::get('/users/{user}/edit',[UsersController::class,'edit'])->name('users.edit');
-    Route::put('/users/{user}/edit',[UsersController::class,'update'])->name('users.update');
-    Route::delete('/users/{user}/delete',[UsersController::class,'destroy'])->name('users.destroy');
-    ROute::put('/users/{user}/active',[UsersController::class,'active'])->name('users.active');
+    Route::get('/users', [UsersController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UsersController::class, 'create'])->name('users.create');
+    Route::get('/users/{user}/show', [UsersController::class, 'show'])->name('users.show');
+    Route::post('/users/create', [UsersController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}/edit', [UsersController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}/edit', [UsersController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}/delete', [UsersController::class, 'destroy'])->name('users.destroy');
+    ROute::put('/users/{user}/active', [UsersController::class, 'active'])->name('users.active');
+
+    //Routes Institution
+    Route::get('/institutions', [InstitutionController::class, 'index'])->name('institutions.index');
+    Route::get('/institutions/{institution}/edit', [InstitutionController::class, 'edit'])->name('institutions.edit');
+    Route::put('/institutions/{institution}', [InstitutionController::class, 'update'])->name('institutions.update');
+    Route::delete('/institutions/{institution}', [InstitutionController::class, 'destroy'])->name('institutions.destroy');
+    Route::put('/institutions/{institution}/active', [InstitutionController::class, 'active'])
+        ->name('institutions.active');
 });
 
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
