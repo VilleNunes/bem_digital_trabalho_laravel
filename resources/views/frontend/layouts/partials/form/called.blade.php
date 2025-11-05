aa490f62b460865af500e95f1656be06ee253d95
Maria Eduarda <marizari2006@gmail.com>
2025-11-05 19:47:29 -0300

    called

---
+++ resources/views/frontend/layouts/partials/form/called.blade.php
@@ -0,0 +1,14 @@
+<div class="max-w-6xl mx-auto grid md:grid-cols-2 gap-12 px-6 items-center">
+    
+    <div>
+      <h2 class="text-4xl font-extrabold text-green-900 mb-6">
+        Quer <span class="text-green-600">multiplicar o impacto</span> da sua <span class="text-green-800">instituição?</span>
+      </h2>
+      <p class="text-gray-700 mb-6 text-lg">
+        Preencha o formulário ao lado para cadastrar sua organização e fazer parte do <strong>Bem Digital</strong> — 
+        conectando solidariedade e resultados reais.
+      </p>
+      <button class="bg-green-600 text-white font-semibold px-5 py-3 rounded-lg shadow-md hover:bg-green-700 transition">
+        EXCLUSIVO PARA INSTITUIÇÕES E EMPRESAS
+      </button>
+    </div>
